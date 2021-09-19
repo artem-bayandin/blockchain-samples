ec91e953446d1cf4726932af021b81842c318e9f
Artem Bayandin <bayandin.artem.official@gmail.com>
2021-09-19 21:53:44 +0300

    all the attempts in Raffle3.sol. random is generated, price is requested. needs cleanup.

---
+++ raffle/contracts/Raffle.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Raffle {
+
+    function deposit(address _token, uint256 _amount)
+    public
+    payable {
+        // place a bid
+    }
+
+    function rollTheDice()
+    public {
+        // pause game
+        // roll the dice
+        // get a winner
+        // start new round
+    }
+}
