a6e8b32130eb2d3e7532395d97e804c80505d4d6
Artem Bayandin <bayandin.artem.official@gmail.com>
2021-09-21 21:01:24 +0300

    const erc20tokens moved to a separate folder, as plann to use it in tests

---
+++ raffle/common/deployment.js
@@ -0,0 +1,50 @@
+// proxy addresses are taken from https://docs.chain.link/docs/ethereum-addresses/ for Rinkeby network.
+// if deployed locally, proxy addresses should be updated to point to appropriate local AggregatorV3Mock
+// tokens are named similar to its originals to have less confusion
+// 'token' and 'tokenAddress' should be filled on deployment
+// 'initialProxyValue' is used for locally deployed proxies to set some initial value to be returned
+const erc20Mocks = [{
+    name: 'LINK mock',
+    symbol: 'LINKM',
+    proxy: '0xd8bD0a1cB028a31AA859A21A3758685a95dE4623',
+    decimals: 8,
+    isUsd: true,
+    tokenAddress: null,
+    initialProxyValue: (25 * 10 ** 8).toString()
+}, {
+    name: 'DAI mock',
+    symbol: 'DAIM',
+    proxy: '0x74825DbC8BF76CC4e9494d0ecB210f676Efa001D',
+    decimals: 18,
+    isUsd: false,
+    tokenAddress: null,
+    initialProxyValue: (1 * 10 ** 18).toString()
+}, {
+    name: 'BNB mock',
+    symbol: 'BNBM',
+    proxy: '0xcf0f51ca2cDAecb464eeE4227f5295F2384F84ED',
+    decimals: 8,
+    isUsd: true,
+    tokenAddress: null,
+    initialProxyValue: (367 * 10 ** 8).toString()
+}, {
+    name: 'TRX mock',
+    symbol: 'TRXM',
+    proxy: '0xb29f616a0d54FF292e997922fFf46012a63E2FAe',
+    decimals: 8,
+    isUsd: true,
+    tokenAddress: null,
+    initialProxyValue: (0.09 * 10 ** 8).toString()
+}, {
+    name: 'ZRX mock',
+    symbol: 'ZRXM',
+    proxy: '0xF7Bbe4D7d13d600127B6Aa132f1dCea301e9c8Fc',
+    decimals: 8,
+    isUsd: true,
+    tokenAddress: null,
+    initialProxyValue: (0.92 * 10 ** 8).toString()
+}]
+
+module.exports = {
+    erc20Mocks
+}
